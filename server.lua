3590331cbd1880bc940a8165cc98473454c7c123
fusiondevsfivem <145299854+fusiondevsfivem@users.noreply.github.com>
2023-12-15 08:38:43 -0500

    push

--- server.lua
+++ server.lua
@@ -6,35 +6,58 @@ local GetPlayerName = GetPlayerName
 RegisterNetEvent('Lobbies', function(data)
     if data.type == 'New Lobby' then 
         local index = #Lobbies+1
+        local name = GetPlayerName(source)
+
         if data.Password then 
             Lobbies[index] = {
-                Players = {GetPlayerName(source)},
+                Players = {name},
+                CachedPlayers = {source},
                 index = index,
                 Password = data.Password,
-                Name = data.Name
+                Name = data.Name,
+                source = source,
+                Creator = name,
+                Settings = data.Settings
             }
         else
             Lobbies[index] = {
-                Players = {GetPlayerName(source)},
+                Players = {name},
                 index = index,
-                Name = data.Name
+                CachedPlayers = {source},
+                Name = data.Name,
+                Creator = name,
+                Settings = data.Settings
             }
         end
         SetPlayerRoutingBucket(source, index)
     elseif data.type == 'Leave' then 
         SetPlayerRoutingBucket(source, 0)
+        for i = 1, #Lobbies do 
+            local Lobby = Lobbies[i]
+            if Lobby.Creator == source then 
+                for j = 1, #Lobby.CachedPlayers do 
+                    local Player = Lobby.CachedPlayers[j]
+                    SetPlayerRoutingBucket(Player, 0)
+                    TriggerClientEvent('Lobbies', Player, nil)
+                end
+                Lobby = nil
+            end
+        end 
     elseif data.type == 'Join Lobby' then 
         local lobby = Lobbies[data.Lobby]
         if lobby.Password then 
             local pass = lib.callback.await('Lobby:EnterPassword', source)
             if lobby.Password == pass then 
                 SetPlayerRoutingBucket(source, data.Lobby)
-                lobby.Players = lobby.Players ..' '.. GetPlayerName(source)
+                lobby.Players[#lobby.Players+1] = GetPlayerName(source)
+                lobby.CachedPlayers[lobby.CachedPlayers+1] = source
             end
         else
             SetPlayerRoutingBucket(source, data.Lobby)
-            lobby.Players = lobby.Players ..' '.. GetPlayerName(source)
+            lobby.Players[#lobby.Players+1] = GetPlayerName(source)
+            lobby.CachedPlayers[lobby.CachedPlayers+1] = source
         end
+        TriggerClientEvent('Lobbies', source, lobby.Settings)
     end
 end)
 
@@ -42,4 +65,18 @@ end)
 
 lib.callback.register('GetLobbyCount', function()
     return Lobbies
+end)
+
+AddEventHandler('playerDropped', function ()
+    for i = 1, #Lobbies do 
+        local Lobby = Lobbies[i]
+        if Lobby.Creator == source then 
+            for j = 1, #Lobby.CachedPlayers do 
+                local Player = Lobby.CachedPlayers[j]
+                SetPlayerRoutingBucket(Player, 0)
+                TriggerClientEvent('Lobbies', Player, nil)
+            end
+            Lobby = nil
+        end
+    end 
 end)
